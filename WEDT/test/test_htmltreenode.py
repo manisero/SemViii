a953b58716778b4a752facb94e79f481b3fa2fb3
turekj <jkbturek@gmail.com>
2013-05-03 17:45:34 +0200

    Added tag_attribute_equals() method to HTMLTreeNode

--- WEDT/test/test_htmltreenode.py
+++ WEDT/test/test_htmltreenode.py
@@ -14,6 +14,7 @@
 import unittest
 from tree.htmltreenode import HTMLTreeNode
 
+
 class BaseTreeNodeTest(unittest.TestCase):
     def setUp(self):
             self.root = HTMLTreeNode("root", "rootText", [])
@@ -23,17 +24,30 @@ class BaseTreeNodeTest(unittest.TestCase):
             
             self.root.add_nodes([self.firstRootChild, self.secondRootChild])
             
-            self.firstFirstRootChildChild = HTMLTreeNode("firstFirstRootChildChild", "firstFirstRootChildChildText", [])
-            self.secondFirstRootChildChild = HTMLTreeNode("secondFirstRootChildChild", "secondFirstRootChildChildText", [])
-            self.thirdFirstRootChildChild = HTMLTreeNode("thirdFirstRootChildChild", "thirdFirstRootChildChildText", [])
+            self.firstFirstRootChildChild = HTMLTreeNode("firstFirstRootChildChild",
+                                                         "firstFirstRootChildChildText",
+                                                         [])
+            self.secondFirstRootChildChild = HTMLTreeNode("secondFirstRootChildChild",
+                                                          "secondFirstRootChildChildText",
+                                                          [])
+            self.thirdFirstRootChildChild = HTMLTreeNode("thirdFirstRootChildChild",
+                                                         "thirdFirstRootChildChildText",
+                                                         [])
             
-            self.firstRootChild.add_nodes([self.firstFirstRootChildChild, self.secondFirstRootChildChild, self.thirdFirstRootChildChild])
+            self.firstRootChild.add_nodes([self.firstFirstRootChildChild,
+                                           self.secondFirstRootChildChild,
+                                           self.thirdFirstRootChildChild])
             
-            self.firstSecondRootChildChild = HTMLTreeNode("firstSecondRootChildChild", "firstSecondRootChildChildText", [])
-            self.secondSecondRootChildChild = HTMLTreeNode("secondSecondRootChildChild", "secondSecondRootChildChildText", [])
+            self.firstSecondRootChildChild = HTMLTreeNode("firstSecondRootChildChild",
+                                                          "firstSecondRootChildChildText",
+                                                          [])
+            self.secondSecondRootChildChild = HTMLTreeNode("secondSecondRootChildChild",
+                                                           "secondSecondRootChildChildText",
+                                                           [])
             
             self.secondRootChild.add_nodes([self.firstSecondRootChildChild, self.secondSecondRootChildChild])
-            
+
+
 class AddNodesHTMLTreeNodeTest(BaseTreeNodeTest):
     def runTest(self):
         self.assertEquals(self.root.get_nodes()[0], self.firstRootChild)
@@ -56,7 +70,8 @@ class AddNodesHTMLTreeNodeTest(BaseTreeNodeTest):
         
         self.assertEquals(self.secondRootChild.get_nodes()[1], self.secondSecondRootChildChild)
         self.assertEquals(self.secondSecondRootChildChild.get_parent(), self.secondRootChild)
-            
+
+
 class NextHTMLTreeNodeTest(BaseTreeNodeTest):
     def runTest(self):
         current_node = self.root
@@ -85,6 +100,7 @@ class NextHTMLTreeNodeTest(BaseTreeNodeTest):
         current_node = current_node.next()
         self.assertEquals(current_node, None)
 
+
 class PreviousHTMLTreeNodeTest(BaseTreeNodeTest):
     def runTest(self):
         current_node = self.secondSecondRootChildChild
@@ -112,4 +128,32 @@ class PreviousHTMLTreeNodeTest(BaseTreeNodeTest):
         
         current_node = current_node.previous()
         self.assertEquals(current_node, None)
-        
+
+
+class TagAttributeEqualsHTMLTreeNodeTest(unittest.TestCase):
+    def setUp(self):
+        self.p_blog_entry = HTMLTreeNode('p', 'BlogEntry not bold text', {'class': 'BlogEntry'})
+
+        self.div_blog_entry = HTMLTreeNode('div', 'BlogEntry not bold text', {'class': 'BlogEntry'})
+
+        self.div_blog_entry_bold = HTMLTreeNode('div', 'BlogEntry text', {'class': 'BlogEntry',
+                                                                          'style': 'font-weight:bold'})
+
+        self.div_blog_first_menu_entry = HTMLTreeNode('div', 'First MenuEntry', {'class': 'MenuEntry',
+                                                                                 'font': '1'})
+
+        self.div_blog_second_menu_entry = HTMLTreeNode('div', 'Second MenuEntry', {'class': 'MenuEntry',
+                                                                                   'font': '1'})
+
+    def test_full_positive_match(self):
+        self.assertTrue(self.div_blog_first_menu_entry.tag_attribute_equals(self.div_blog_second_menu_entry))
+
+    def test_partial_positive_match(self):
+        self.assertFalse(self.div_blog_entry.tag_attribute_equals(self.div_blog_entry_bold))
+        self.assertTrue(self.div_blog_entry.tag_attribute_equals(self.div_blog_entry_bold, ['class']))
+
+    def test_negative_tag_match(self):
+        self.assertFalse(self.p_blog_entry.tag_attribute_equals(self.div_blog_entry))
+
+    def test_negative_match(self):
+        self.assertFalse(self.div_blog_entry.tag_attribute_equals(self.div_blog_first_menu_entry))
--- WEDT/tree/htmltreenode.py
+++ WEDT/tree/htmltreenode.py
@@ -96,4 +96,16 @@ class HTMLTreeNode:
             previous_node = previous_node.get_nodes()[-1]
         
         return previous_node
-    
+
+    def tag_attribute_equals(self, other_node, compared_attributes=None):
+
+        stripped_self_attributes = self.get_attributes()
+        stripped_other_attributes = other_node.get_attributes()
+
+        if compared_attributes is not None:
+            stripped_self_attributes = {attribute: self.get_attributes()[attribute]
+                                        for attribute in compared_attributes if attribute in self.get_attributes()}
+            stripped_other_attributes = {attribute: other_node.get_attributes()[attribute] for attribute
+                                         in compared_attributes if attribute in other_node.get_attributes()}
+
+        return self.get_tag() == other_node.get_tag() and stripped_self_attributes == stripped_other_attributes
