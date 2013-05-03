27cd75daa346b0f441314694bb085f8834f4e1ce
turekj <jkbturek@gmail.com>
2013-05-03 18:28:18 +0200

    Created HTMLTreeBrowser class for HTML tree utility methods

---
+++ WEDT/test/test_htmltreebrowser.py
@@ -0,0 +1,53 @@
+"""
+    C{HTMLTreeBrowser} unit tests. Function covered:
+
+        - C{get_repeated_nodes(html_tree, minimum_repeats, valid_tags, valid_attributes)}
+
+    @author: Jakub Turek
+    @contact: jkbturek(at)gmail(dot)com
+    @date: 28-04-2013
+    @version: 1.0
+"""
+import unittest
+from tree.htmltreebrowser import HTMLTreeBrowser
+from tree.htmltreenode import HTMLTreeNode
+
+
+class GetRepeatedNodesHTMLTreeBrowserTest(unittest.TestCase):
+    def setUp(self):
+        self.tree_browser = HTMLTreeBrowser()
+
+        self.root = HTMLTreeNode('div', 'Parent', {'class': 'BlogParent'})
+
+        self.first_entry = HTMLTreeNode('p', 'FirstEntry', {'class': 'BlogEntry'})
+        self.second_entry = HTMLTreeNode('p', 'SecondEntry', {'class': 'BlogEntry'})
+        self.third_entry = HTMLTreeNode('p', 'ThirdEntry', {'class': 'BlogEntry'})
+        self.footer = HTMLTreeNode('div', 'Footer', {'class': 'Footer'})
+
+        self.root.add_nodes([self.first_entry, self.second_entry, self.third_entry, self.footer])
+
+        self.first_date = HTMLTreeNode('span', 'FirstDate', {'class': 'BlogDate'})
+        self.second_date = HTMLTreeNode('span', 'SecondDate', {'class': 'BlogDate'})
+        self.first_comments = HTMLTreeNode('a', 'FirstComment', {'class': 'BlogComments'})
+
+        self.first_entry.add_node(self.first_date)
+        self.second_entry.add_node(self.first_comments)
+        self.third_entry.add_node(self.second_date)
+
+    def runTest(self):
+        repeated_nodes = self.tree_browser.get_repeated_nodes(self.root, 2)
+
+        self.assertEquals(len(repeated_nodes), 2)
+
+        self.assertIn(self.first_entry, repeated_nodes)
+        self.assertIn(self.first_date, repeated_nodes)
+
+        self.assertEquals(len(repeated_nodes[self.first_entry]), 3)
+        self.assertEquals(len(repeated_nodes[self.first_date]), 2)
+
+        self.assertIn(self.first_entry, repeated_nodes[self.first_entry])
+        self.assertIn(self.second_entry, repeated_nodes[self.first_entry])
+        self.assertIn(self.third_entry, repeated_nodes[self.first_entry])
+
+        self.assertIn(self.first_date, repeated_nodes[self.first_date])
+        self.assertIn(self.second_date, repeated_nodes[self.first_date])
---
+++ WEDT/tree/htmltreebrowser.py
@@ -0,0 +1,40 @@
+'''
+    HTML tree browser. Implements searching algorithms in C{HTMLTreeNode} based structure.
+
+    @author: Jakub Turek
+    @contact: jkbturek(at)gmail(dot)com
+    @date: 03-05-2013
+    @version: 1.0
+'''
+
+
+class HTMLTreeBrowser:
+    def get_repeated_nodes(self, html_tree, minimum_repeats, valid_tags=None, valid_attributes=None):
+        repeated_nodes = {}
+        current_node = html_tree
+
+        while current_node is not None:
+            if valid_tags is not None and current_node.get_tag() not in valid_tags:
+                continue
+
+            repeated_nodes_entry = self.__get_repeated_nodes_entry(current_node, repeated_nodes, valid_attributes)
+
+            if repeated_nodes_entry is not None:
+                repeated_nodes[repeated_nodes_entry].append(current_node)
+            else:
+                repeated_nodes[current_node] = []
+                repeated_nodes[current_node].append(current_node)
+
+            current_node = current_node.next()
+
+        repeated_nodes = {node: repeated_nodes[node] for node in repeated_nodes.keys()
+                          if len(repeated_nodes[node]) >= minimum_repeats}
+
+        return repeated_nodes
+
+    def __get_repeated_nodes_entry(self, node, repeated_nodes, valid_attributes):
+        for key in repeated_nodes.keys():
+            if key.tag_attribute_equals(node, valid_attributes):
+                return key
+
+        return None
