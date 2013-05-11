610bd71ebb0ddf2c0931fd64b082038cba5bac1e
manisero <michalaniserowicz@gmail.com>
2013-05-11 20:33:09 +0200

    Improved length validation tests

--- MBI/MBI.Logic.Tests/ScaffoldValidatorTests/LengthValidationTests.cs
+++ MBI/MBI.Logic.Tests/ScaffoldValidatorTests/LengthValidationTests.cs
@@ -20,15 +20,19 @@ namespace MBI.Logic.Tests.ScaffoldValidatorTests
 		}
 
 		[Test]
-		public void rejects_two_pets_of_different_lengths_placed_in_the_same_configs()
+		[Sequential]
+		public void rejects_two_pets_of_different_lengths_placed_in_the_same_configs(
+			[Values(10, 10)] int pet1Length,
+			[Values(20, 10)] int pet2Length,
+			[Values(4, 8)] int expectedResult)
 		{
 			// Arrange
 			var contigs = new[] { "aabb", "ccdd" };
-			var pet1 = new PairedEndTag { Beginning = "aa", End = "cc", Length = 10 };
-			var pet2 = new PairedEndTag { Beginning = "bb", End = "dd", Length = 20 };
+			var pet1 = new PairedEndTag { Beginning = "aa", End = "cc", Length = pet1Length };
+			var pet2 = new PairedEndTag { Beginning = "bb", End = "dd", Length = pet2Length };
 
 			// Act & Assert
-			TestValidate(contigs, new[] { pet1, pet2 }, 4);
+			TestValidate(contigs, new[] { pet1, pet2 }, expectedResult);
 		}
 	}
 }
