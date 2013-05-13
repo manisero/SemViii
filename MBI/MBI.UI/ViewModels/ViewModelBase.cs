2c94dce195102db4eb684985c645333f4714e1ac
manisero <michalaniserowicz@gmail.com>
2013-05-14 00:21:06 +0200

    Added messages to UI

---
+++ MBI/MBI.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace MBI.UI.ViewModels
+{
+	public abstract class ViewModelBase : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void NotifyPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged == null)
+			{
+				return;
+			}
+
+			PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
